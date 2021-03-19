9a03a1e6ef41bdfda890d9068e149eb176436c2a
lvl99magic <80277111+lvl99magic@users.noreply.github.com>
2021-03-18 18:32:32 -0700

    Add files via upload

---
+++ SqlConn.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+import pyodbc
+
+server = 'WSAMZN-IBOS3L3K\MSSQLSERVER10'
+db = 'AdventureWorks2019'
+driver = 'SQL Server Native Client RDA 11.0'
+
+sqlcon = create_engine('mssql+pyodbc://@' + server + '/' + db + '?trusted_connection=yes&driver=' + driver + '', fast_executemany=True)
+
+
+# execute many at the end of sqlalchemy "create engine"
